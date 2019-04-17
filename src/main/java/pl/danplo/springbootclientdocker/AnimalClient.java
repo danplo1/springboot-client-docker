ddbf53d05771417ef591d2b5c1e29390d8a468b1
danplo1 <budweiser401@wp.pl>
2019-04-17 12:11:49 +0200

    Initial commit

---
+++ src/main/java/pl/danplo/springbootclientdocker/AnimalClient.java
@@ -0,0 +1,30 @@
+package pl.danplo.springbootclientdocker;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class AnimalClient {
+
+
+    @GetMapping("/showForGui")
+    public  Animal[] get(){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Animal[]> exchange = restTemplate.exchange("http://localhost:10101/animals",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                Animal[].class);
+
+        Animal[] body = exchange.getBody();
+        return body;
+
+    }
+}
---
+++ src/main/java/pl/danplo/springbootclientdocker/SpringbootClientDockerApplication.java
@@ -0,0 +1,15 @@
+package pl.danplo.springbootclientdocker;
+
+//Docker Compose - zarządzanie obrazami Docker yt p.bykowski channel
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClientDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootClientDockerApplication.class, args);
+	}
+
+}
